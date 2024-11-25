cf2309d225fe3c74f6dfea240c1d8211763c0746
nhdang05 <MAY 06@LAB2-08>
2024-11-25 10:42:27 +0700

    Add project files.

---
+++ Bai19_InSoNguyenTo/Bai19_InSoNguyenTo.cpp
@@ -0,0 +1,27 @@
+// Bai19_InSoNguyenTo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << " KIEM TRA SO CO PHAI SO NGUYEN TO KHONG !\n";
+	int	so;
+	cout << " Nhap So: "; cin >> so;
+	int dem = 0;
+	for (int i = 1; i <= so; i++)
+	{
+		// neu chia het thi tang dem len 1 : so % i == 0
+		if (so % i == 0)
+		{
+			dem++;
+		}
+	}
+	cout << " so dem = " << dem << endl;
+	if (dem == 2) {
+		cout << " so " << so << " la so nguyen to \n";
+	}
+	else {
+		cout << " so " << so << " khong phai so nguyen to \n";
+	}
+	return 0;
+}
